61761b4580b435e749cae90c63bfe7638c0d533d
notreteams-boop <notreteams@gmail.com>
2026-05-05 23:37:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ import google.generativeai as genai
 st.set_page_config(page_title="AI Exam Prep", page_icon="📝")
 
 # Твой рабочий ключ
-API_KEY = "AIzaSyBEgXRMal1511eD3H9mq5V7dKBTNPPTuLQ"
+API_KEY = "AIzaSyBsETc7a3v_z98gmhDQPgKWo2WWUM7bzFg"
 genai.configure(api_key=API_KEY)
 
 # Указываем конкретную рабочую модель из твоего списка
 # Мы выбрали gemini-2.0-flash, так как она самая надежная
-MODEL_NAME = 'models/gemini-2.0-flash'
+MODEL_NAME = 'models/gemini-1.5-flash'
 model = genai.GenerativeModel(MODEL_NAME)
 
 # --- 2. ЗАГРУЗКА ТЕМ ---

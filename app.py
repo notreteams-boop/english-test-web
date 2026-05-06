89be9acf998801d4a8bc43c379f9ca37b9e6ed40
notreteams-boop <notreteams@gmail.com>
2026-05-06 21:00:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ st.markdown("""
 # --- 2. API SETUP ---
 try:
     genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-    model = genai.GenerativeModel('gemini-1.5-flash-latest')
+    model = genai.GenerativeModel('gemini-1.5-pro')
 except:
     st.error("API Key error! Please check your Streamlit Secrets.")
     st.stop()

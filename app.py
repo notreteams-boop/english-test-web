4afc0ff764ba5b35aa1646753b285ca5ea295216
notreteams-boop <notreteams@gmail.com>
2026-05-06 21:14:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ st.markdown("""
 # --- 2. API SETUP ---
 try:
     genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-    model = genai.GenerativeModel('gemini-1.5-flash-latest')
+    model = genai.GenerativeModel('gemini-1.0-pro')
 except:
     st.error("API Key Error!")
     st.stop()

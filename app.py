8b00bb4bbcfe818dfef18391c66d2268b25b148c
notreteams-boop <notreteams@gmail.com>
2026-05-07 22:08:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+st.markdown("""
+<style>
+    .stApp { background-color: #ffffff; }
+</style>
+""", unsafe_allow_html=True)
 # Используем session_state, чтобы сайт запомнил, какую кнопку нажал пользователь
 if 'choice' not in st.session_state:
     st.session_state.choice = 'main'
